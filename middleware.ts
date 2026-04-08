c913d70c963208c8c835cacecc18553c81130b06
Admin <rahul.k@simplilearn.net>
2026-04-08 20:14:54 +0530

    update and fixed bugs

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,11 @@
 import NextAuth from "next-auth"
 import { authConfig } from "@/lib/auth.config"
 
-export const { auth: middleware } = NextAuth(authConfig)
+const { auth } = NextAuth(authConfig)
+
+export default auth((req) => {
+  return undefined
+})
 
 export const config = {
   matcher: [
